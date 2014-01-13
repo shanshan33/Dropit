66206968087f4b95f7234536c13709d90bc9ab2e
Shanshan ZHAO <chezzhaoss@shanshan.local>
2014-01-14 00:53:16 +0100

    Initial Commit

---
+++ Dropit/DropitViewController.m
@@ -0,0 +1,29 @@
+//
+//  DropitViewController.m
+//  Dropit
+//
+//  Created by Shanshan ZHAO on 14-1-14.
+//  Copyright (c) 2014年 Shanshan ZHAO. All rights reserved.
+//
+
+#import "DropitViewController.h"
+
+@interface DropitViewController ()
+
+@end
+
+@implementation DropitViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ Dropit/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Dropit
+//
+//  Created by Shanshan ZHAO on 14-1-14.
+//  Copyright (c) 2014年 Shanshan ZHAO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "DropitAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([DropitAppDelegate class]));
+    }
+}
