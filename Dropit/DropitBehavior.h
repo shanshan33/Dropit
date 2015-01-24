6f8dbadb67575c09a81eb552ac1dbd5fd43ec731
Shanshan ZHAO <chezzhaoss@gmail.com>
2015-01-24 15:19:52 +0100

    play with drop animation

---
+++ Dropit/DropitBehavior.h
@@ -0,0 +1,13 @@
+//
+//  DropitBehavior.h
+//  Dropit
+//
+//  Created by Shanshan ZHAO on 14-1-14.
+//  Copyright (c) 2014年 Shanshan ZHAO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DropitBehavior : UIDynamicBehavior
+
+@end
---
+++ Dropit/DropitBehavior.m
@@ -0,0 +1,13 @@
+//
+//  DropitBehavior.m
+//  Dropit
+//
+//  Created by Shanshan ZHAO on 14-1-14.
+//  Copyright (c) 2014年 Shanshan ZHAO. All rights reserved.
+//
+
+#import "DropitBehavior.h"
+
+@implementation DropitBehavior
+
+@end
